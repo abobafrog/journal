3e5d47685e90f5634656b510e2d52b51c8804574
abobafrog <105128226+abobafrog@users.noreply.github.com>
2025-12-02 22:38:37 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 import os
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")          # Telegram токен
-DATABASE_URL = os.getenv("DATABASE_URL")    # PostgreSQL URL
+BOT_TOKEN = os.getenv("8463443445:AAGKv-OLXPBiVYvWFqoUv5-Fmiez2ECLspo")          # Telegram токен
+DATABASE_URL = os.getenv("yamabiko.proxy.rlwy.net")    # PostgreSQL URL
